bfd5d63d17766e89b9d1032788117d8376aedee8
Sufian Suni <sufiansuni@gmail.com>
2022-01-06 14:40:33 +0800

    first implementation

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Page Controller
+class PagesController < ApplicationController
+  def home; end
+
+  def search
+    uri = URI("https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=#{params[:longitude]},#{params[:latitude]}&access_token=#{ENV['MAPBOX_API_KEY']}")
+    res = Net::HTTP.get_response(uri)
+    res_hash = JSON.parse(res.body)
+    if res.code == '200'
+      new_hash = {}
+      res_hash['features'].each do |feature|
+        postal_code = feature['context'][0]['text']
+        unless new_hash.keys.include?(postal_code)
+          new_hash[postal_code] = []
+        end
+        new_hash[postal_code] << feature['text']
+      end
+      render json: new_hash
+    else
+      render json: res_hash
+    end
+  end
+end
